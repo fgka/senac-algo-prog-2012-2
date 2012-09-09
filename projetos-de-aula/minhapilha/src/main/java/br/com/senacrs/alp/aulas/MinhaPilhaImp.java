374ca4488f9e178729fca27c925f748fe8a94ade
fgka <gustavo.andriotti@gmail.com>
2012-09-09 13:33:23 -0300

    Implementação baseada em MinhaLista criada

---
+++ projetos-de-aula/minhapilha/src/main/java/br/com/senacrs/alp/aulas/MinhaPilhaImp.java
@@ -0,0 +1,16 @@
+package br.com.senacrs.alp.aulas;
+
+public class MinhaPilhaImp<T> extends AbstractMinhaPilha<T> {
+	
+	public MinhaPilhaImp() {
+
+		super();
+	}
+
+	@Override
+	protected MinhaLista<T> getMinhaLista() {
+
+		return null;
+	}
+
+}
---
+++ projetos-de-aula/minhapilha/src/test/java/br/com/senacrs/alp/aulas/MinhaPilhaImpTest.java
@@ -0,0 +1,29 @@
+package br.com.senacrs.alp.aulas;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class MinhaPilhaImpTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGetMinhaLista() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMinhaPilhaImp() {
+		fail("Not yet implemented");
+	}
+
+}

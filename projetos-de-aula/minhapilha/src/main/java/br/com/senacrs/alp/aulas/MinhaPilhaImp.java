1924bfe18f606a96ffca1db43ee9d560b7c47000
fgka <gustavo.andriotti@gmail.com>
2012-09-09 13:38:47 -0300

    MinhaPilhaImp implementada.

--- projetos-de-aula/minhapilha/src/main/java/br/com/senacrs/alp/aulas/MinhaPilhaImp.java
+++ projetos-de-aula/minhapilha/src/main/java/br/com/senacrs/alp/aulas/MinhaPilhaImp.java
@@ -2,15 +2,19 @@ package br.com.senacrs.alp.aulas;
 
 public class MinhaPilhaImp<T> extends AbstractMinhaPilha<T> {
 	
+	private MinhaLista<T> lista = null;
+	
 	public MinhaPilhaImp() {
 
 		super();
+		
+		this.lista = new MinhaListaImp<T>();
 	}
 
 	@Override
 	protected MinhaLista<T> getMinhaLista() {
 
-		return null;
+		return this.lista;
 	}
 
 }

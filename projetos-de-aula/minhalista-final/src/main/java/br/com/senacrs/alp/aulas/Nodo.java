d65e29ef47585646d12c57784ebf2978e95ba156
fgka <gustavo.andriotti@gmail.com>
2012-09-18 23:17:11 -0300

    toString() criado.

--- projetos-de-aula/minhalista-final/src/main/java/br/com/senacrs/alp/aulas/Nodo.java
+++ projetos-de-aula/minhalista-final/src/main/java/br/com/senacrs/alp/aulas/Nodo.java
@@ -4,23 +4,39 @@ public class Nodo<Tipo extends Object> {
 
 	private Tipo valor;
 	private Nodo<Tipo> proximo = null;
-	
+
 	public Nodo(Tipo valor) {
 
 		this.valor = valor;
 	}
+
 	public Tipo getValor() {
 		return valor;
 	}
+
 	public void setValor(Tipo valor) {
 		this.valor = valor;
 	}
+
 	public Nodo<Tipo> getProximo() {
 		return proximo;
 	}
+
 	public void setProximo(Nodo<Tipo> proximo) {
 		this.proximo = proximo;
 	}
-	
-	
+
+	@Override
+	public String toString() {
+		
+		String resultado = null;
+		
+		resultado = "[" + this.valor + "]";
+		if (this.proximo != null) {
+			resultado += "->";
+		}
+		
+		return resultado;
+	}
+
 }

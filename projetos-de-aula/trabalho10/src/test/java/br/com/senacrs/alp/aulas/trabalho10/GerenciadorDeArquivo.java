b70f0888024561cfaca392e0926890550450fe4c
fgka <gustavo.andriotti@gmail.com>
2012-10-18 21:55:21 -0300

    Esqueleto da classe

---
+++ projetos-de-aula/trabalho10/src/test/java/br/com/senacrs/alp/aulas/trabalho10/GerenciadorDeArquivo.java
@@ -0,0 +1,31 @@
+package br.com.senacrs.alp.aulas.trabalho10;
+
+public class GerenciadorDeArquivo {
+	
+	public GerenciadorDeArquivo(String arquivo) {
+		// TODO
+	}
+	
+	public String[] lerArquivo() {
+		
+		String[] resultado = null;
+		
+		// TODO 
+		
+		return resultado;
+	}
+	
+	public String[] lerArquivoComSubstituicao(String lido, String retornado) {
+		
+		String[] resultado = null;
+		
+		// TODO 
+		
+		return resultado;
+	}
+	
+	public void escreverArquivo(String[] conteudo) {
+		
+		// TODO
+	}
+}

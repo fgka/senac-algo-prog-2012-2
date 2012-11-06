9a45690ac6ac4dcbc17fa6a4d7f093904db90c9b
gkandriotti <gkandriotti@Sala603-PROF.educcbx.senacrs.com.br>
2012-11-05 22:03:57 -0200

    Implimentação inicial

---
+++ projetos-de-aula/trabalho12/src/main/java/br/com/senacrs/alp/aulas/trabalho12/Main.java
@@ -0,0 +1,15 @@
+package br.com.senacrs.alp.aulas.trabalho12;
+
+import java.util.Arrays;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		String msg = null;
+		
+		msg = "Argumentos: ";
+		msg += Arrays.toString(args);		
+		System.out.println(msg);
+	}
+}
